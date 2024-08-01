1bc3aa43a576d8fb74388f37749686cc49d36ebc
miserylee <miserylee@foxmail.com>
2024-08-01 17:13:33 +0800

    fix: add eslint-patch modern-module-resolution

--- packages/configs/.eslintrc.js
+++ packages/configs/.eslintrc.js
@@ -1,10 +1,13 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
   ],
-  plugins: ['prettier', 'react', 'react-hooks', 'import', 'promise'],
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks', 'import', 'promise'],
   parser: '@typescript-eslint/parser',
   settings: {
     'import/resolver': {
